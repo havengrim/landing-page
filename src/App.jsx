c62c1d75274db069d4f274d1e404a813a26e1439
angeloguiao <angeloguiao.gci@gmail.com>
2024-07-19 15:05:47 +0800

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Navbar } from './components'
 
 function App() {
 
   return (
-    <>
-      <p className=' text-red-500'>hello</p>
-    </>
+    <div className=' bg-[#E2E2E2]'>
+      <Navbar />
+    </div>
   )
 }
 
